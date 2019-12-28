5715d2586e2a120604d60fb44cdc3bb902f33c93
Rahul Soni <soni_rahul@live.com>
2019-12-29 02:26:55 +0530

    Added the following features
    1. Spring boot admin
    2. Spring boot cloud config
    3. Eureka

--- src/main/java/com/altimetrik/registry/RegistryApplication.java
+++ src/main/java/com/altimetrik/registry/RegistryApplication.java
@@ -1,9 +1,17 @@
 package com.altimetrik.registry;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+@EnableAdminServer
+@EnableConfigServer
 public class RegistryApplication {
 
 	public static void main(String[] args) {
